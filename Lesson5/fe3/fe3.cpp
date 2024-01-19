74250fcb6e769c123d2d3fac6f48bef887a64d1a
ezratweaver <ezratweaver@gmail.com>
2024-01-19 14:29:26 -0500

    Finger exercise commit

---
+++ Lesson5/fe3/fe3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+consteval auto compileTime(auto value)
+{
+    return value;
+}
+
+constexpr int greater(int x, int y)
+{
+    return (x > y ? x : y);
+}
+
+int main()
+{
+    std::cout << greater(5, 6) << "\n";
+    std::cout << compileTime(greater(5, 6)) << "\n";
+
+    int x { 5 };
+    std::cout << greater(x, 6) << "\n";
+
+    return 0;
+}
+
