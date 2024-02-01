61724413b3a3e5a0431ae816a2cd1f969bc540d0
ezratweaver <ezratweaver@gmail.com>
2024-02-01 12:41:32 -0500

    Finger exercise commit

---
+++ Chapter9/fe1/hi_lo_2.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <limits>
+#include "random.h"
+
+void ignoreLine()
+{
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int getGameNumber()
+{
+    return Random::get(1, 100);
+}
+
+const char* higherOrLowerStr(int userGuess, int gameNumber)
+{
+    if (userGuess > gameNumber) 
+        return "Your guess is too high.\n";
+    if (userGuess < gameNumber)
+        return "Your guess is too low.\n";
+    else
+        return "Your guess was correct, you win!\n";
+}
+
+int higherOrLowerLogic(int userGuess, int gameNumber)
+{
+    if (userGuess == gameNumber)
+    {
+        std::cout << "Correct! You win! The number was " << gameNumber << ".\n";
+        return 1;
+    }
+    if (userGuess < 0)
+    {
+        std::cout << "Sorry, you lose. The correct number was " << gameNumber << ".\n";
+        return -1;
+    }
+    return 0;
+}
+
+int getUserGuess(int guessCount)
+{
+    if (guessCount > 7)
+        return -1;
+
+    while (true)
+    {
+        
+        std::cout << "Guess #" << guessCount << ": ";
+
+        int userGuess{ };
+        std::cin >> userGuess;
+        
+        if (!std::cin)
+        {
+            if (std::cin.eof())
+            {
+                exit(0);
+            }
+
+            std::cin.clear();
+            ignoreLine();    
+            std::cout << "Oops, that input is invalid. Please try again\n";
+        }
+        else if (userGuess > 100 || userGuess < 0) {
+            std::cout << "Please enter a number between 1-100.\n";
+        } 
+        else
+        {
+            ignoreLine();
+            return userGuess;
+        }
+        
+
+    }
+}
+
+void playGame()
+{
+    int gameNumber{ getGameNumber() };
+
+    int guessCount{ 1 };
+
+    std::cout << "Lets play a game. Im thinking of a number "
+                 "between 1 and 100. You have 7 tries to guess what it is\n";
+    while (true)
+    {
+        int userGuess { getUserGuess(guessCount) }; 
+        ++guessCount;
+        std::cout << higherOrLowerStr(userGuess, gameNumber);
+
+        if (higherOrLowerLogic(userGuess, gameNumber) != 0)
+            break;
+    }
+}
+
+int main()
+{
+
+main:    
+
+    playGame();
+
+    while (true)
+    {
+
+        std::cout << "Would you like to play again (y/n)? ";
+        
+        char userPrompt{ };
+        std::cin >> userPrompt;
+
+        if (userPrompt == 'y')
+        {
+            ignoreLine();
+            goto main;
+        }
+        else if (userPrompt == 'n')
+        {
+            std::cout << "Thank you for playing.\n"; 
+            break;
+        }
+        else
+        {
+            ignoreLine();
+            continue;
+        }
+
+    }
+
+}
+
