b3cb6d5eb16b6638a4118b931c628361814d61f3
ezratweaver <ezratweaver@gmail.com>
2024-02-10 13:10:48 -1000

    Fixed quiz to reflect problem given

--- Chapter13/fe5/struct_quiz.cpp
+++ Chapter13/fe5/struct_quiz.cpp
@@ -9,21 +9,39 @@ struct AdSense
 };
 
 
+AdSense getAdSenseData()
+{
+    int adsWatched;
+    float clickRate;
+    float avrgEarningsPerClick;
+    std::cout << "Enter total ads watched: ";
+    std::cin >> adsWatched; 
+    std::cout << "Enter click rate (float): ";
+    std::cin >> clickRate;
+    std::cout << "Enter average earnings per click (float): ";
+    std::cin >> avrgEarningsPerClick;
+
+    return { adsWatched, clickRate, avrgEarningsPerClick };
+}
+
+
 void printAdSense(AdSense& adObj)
 {
-    std::cout << "Total Ads Watched: " << adObj.adsWatched << '\n';
+    std::cout << "\nTotal Ads Watched: " << adObj.adsWatched << '\n';
     std::cout << "Ad click rate: " << adObj.clickRate << '\n';
     std::cout << "Average earnings per click: " << adObj.avrgEarningsPerClick << "\n\n";
+
+    double dailyEarnings { adObj.adsWatched * adObj.clickRate * adObj.avrgEarningsPerClick };
+
+    std::cout << "Total daily earnings: " << dailyEarnings << "\n\n";
 }
 
 
 int main()
 {
-    AdSense googleAd { 5, 0.50, 6.7 };
-    AdSense facebookAd {100, 0.90, 10.5 };
 
-    printAdSense(googleAd);
-    printAdSense(facebookAd);
+    AdSense userObj { getAdSenseData() };
+    printAdSense(userObj);
 
     return 0;
 }
